d8065abfcdfbbd80fb577da51ef665a599a051e1
root <root@LAPTOP-V2AU3D56>
2023-11-17 21:25:37 +0800

    update .gitignore

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2021/3/22
-# @Author  : Aspen Stars
-# @Contact : aspenstars@qq.com
-# @FileName: __init__.py
-
-from .JSModel import *
-from .KGModel import *
-from .baseline import *
